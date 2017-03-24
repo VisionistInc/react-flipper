40ce60ab0989cdad44bb41a66cf0be7e47b68f9b
Enrique Caballero <ecaballerojr@icloud.com>
2017-03-23 21:59:03 -0400

    Updated .eslintrc

--- install.js
+++ install.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-var, camelcase, import/newline-after-import */
 
-const child_process = require ('child_process');
+const child_process = require('child_process');
 
 const packages = [
   'babel-core',
@@ -13,12 +13,9 @@ const packages = [
   'babel-preset-react',
   'css-loader',
   'eslint',
-  'eslint-config-airbnb',
-  'eslint-plugin-babel',
-  'eslint-plugin-flowtype',
-  'eslint-plugin-import',
-  'eslint-plugin-jsx-a11y',
-  'eslint-plugin-react',
+  'eslint',
+  'eslint-config-prettier',
+  'eslint-config-react-app',
   'file-loader',
   'flow-bin',
   'less',
@@ -28,28 +25,26 @@ const packages = [
   'url-loader',
   'webpack',
   'webpack-dashboard',
-  'webpack-dev-server'
+  'webpack-dev-server',
 ];
 
-const argv = process.argv.slice (2);
-const isYarn = argv.indexOf ('--yarn') !== -1;
-const isLatest = argv.indexOf ('--latest') !== -1;
+const argv = process.argv.slice(2);
+const isYarn = argv.indexOf('--yarn') !== -1;
+const isLatest = argv.indexOf('--latest') !== -1;
 
-function getPackages () {
-  return packages.map ((package) => {
-    if (package === 'react-hot-loader@next') return package;
-    return isLatest ? (package + '@latest') : package;
+function getPackages() {
+  return packages.map(_p => {
+    if (_p === 'react-hot-loader@next') return _p;
+    return isLatest ? _p + '@latest' : _p;
   });
 }
 
 if (isYarn) {
-  child_process.spawn (
-    'yarn', [ 'add', '-D' ].concat (getPackages ()),
-    { stdio: 'inherit' }
-  );
+  child_process.spawn('yarn', ['add', '-D'].concat(getPackages()), {
+    stdio: 'inherit',
+  });
 } else {
-  child_process.spawn (
-    'npm', [ 'i', '-D' ].concat (getPackages ()),
-    { stdio: 'inherit' }
-  );
+  child_process.spawn('npm', ['i', '-D'].concat(getPackages()), {
+    stdio: 'inherit',
+  });
 }
