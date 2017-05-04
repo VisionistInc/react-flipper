a8280dd56df0bb05fd185281de644eb6d4758423
Enrique Caballero <ecaballerojr@icloud.com>
2017-05-04 19:02:49 -0400

    Ran prettier across Javascript files

--- install.js
+++ install.js
@@ -1,55 +1,55 @@
 /* eslint-disable no-var, camelcase, import/newline-after-import */
 
-const child_process = require('child_process');
+const child_process = require ("child_process");
 
 const packages = [
-  'babel-core',
-  'babel-eslint',
-  'babel-loader',
-  'babel-plugin-add-module-exports',
-  'babel-plugin-transform-class-properties',
-  'babel-plugin-transform-object-rest-spread',
-  'babel-preset-es2015',
-  'babel-preset-react',
-  'css-loader',
-  'eslint',
-  'eslint',
-  'eslint-config-prettier',
-  'eslint-config-react-app',
-  'eslint-plugin-babel',
-  'eslint-plugin-flowtype',
-  'eslint-plugin-import',
-  'eslint-plugin-jsx-a11y',
-  'eslint-plugin-react',
-  'file-loader',
-  'flow-bin',
-  'less',
-  'less-loader',
-  'react-hot-loader@next', // RHL-3 is currently in beta
-  'style-loader',
-  'url-loader',
-  'webpack',
-  'webpack-dashboard',
-  'webpack-dev-server',
+  "babel-core",
+  "babel-eslint",
+  "babel-loader",
+  "babel-plugin-add-module-exports",
+  "babel-plugin-transform-class-properties",
+  "babel-plugin-transform-object-rest-spread",
+  "babel-preset-es2015",
+  "babel-preset-react",
+  "css-loader",
+  "eslint",
+  "eslint",
+  "eslint-config-prettier",
+  "eslint-config-react-app",
+  "eslint-plugin-babel",
+  "eslint-plugin-flowtype",
+  "eslint-plugin-import",
+  "eslint-plugin-jsx-a11y",
+  "eslint-plugin-react",
+  "file-loader",
+  "flow-bin",
+  "less",
+  "less-loader",
+  "react-hot-loader@next", // RHL-3 is currently in beta
+  "style-loader",
+  "url-loader",
+  "webpack",
+  "webpack-dashboard",
+  "webpack-dev-server"
 ];
 
-const argv = process.argv.slice(2);
-const isYarn = argv.indexOf('--yarn') !== -1;
-const isLatest = argv.indexOf('--latest') !== -1;
+const argv = process.argv.slice (2);
+const isYarn = argv.indexOf ("--yarn") !== -1;
+const isLatest = argv.indexOf ("--latest") !== -1;
 
-function getPackages() {
-  return packages.map(_p => {
-    if (_p === 'react-hot-loader@next') return _p;
-    return isLatest ? _p + '@latest' : _p;
+function getPackages () {
+  return packages.map (_p => {
+    if (_p === "react-hot-loader@next") return _p;
+    return isLatest ? _p + "@latest" : _p;
   });
 }
 
 if (isYarn) {
-  child_process.spawn('yarn', ['add', '-D'].concat(getPackages()), {
-    stdio: 'inherit',
+  child_process.spawn ("yarn", [ "add", "-D" ].concat (getPackages ()), {
+    stdio: "inherit"
   });
 } else {
-  child_process.spawn('npm', ['i', '-D'].concat(getPackages()), {
-    stdio: 'inherit',
+  child_process.spawn ("npm", [ "i", "-D" ].concat (getPackages ()), {
+    stdio: "inherit"
   });
 }
--- webpack.config.dist.babel.js
+++ webpack.config.dist.babel.js
@@ -1,52 +1,52 @@
 /* eslint-disable no-console, import/no-unresolved */
 
-import webpack from 'webpack';
-import path from 'path';
-import { ROOT } from './config';
+import webpack from "webpack";
+import path from "path";
+import { ROOT } from "./config";
 
 export const webpackDistConfig = {
   entry: [
-    path.resolve (ROOT, 'src/root.jsx'),
-    path.resolve (ROOT, 'src/index.html')
+    path.resolve (ROOT, "src/root.jsx"),
+    path.resolve (ROOT, "src/index.html")
   ],
   output: {
-    filename: 'bundle.js',
-    path: path.resolve (ROOT, 'dist'),
-    publicPath: '/'
+    filename: "bundle.js",
+    path: path.resolve (ROOT, "dist"),
+    publicPath: "/"
   },
-  devtool: 'source-map',
+  devtool: "source-map",
   resolve: {
-    extensions: [ '*', '.js', '.jsx' ]
+    extensions: [ "*", ".js", ".jsx" ]
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)?/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loader: "babel-loader"
       },
       {
         test: /\.(ico|html)$/,
-        loader: 'file-loader',
+        loader: "file-loader",
         options: {
-          name: '[name].[ext]'
+          name: "[name].[ext]"
         }
       },
       {
         test: /\.(css|less)$/,
         loaders: [
-          'style-loader',
-          'css-loader',
-          'less-loader'
+          "style-loader",
+          "css-loader",
+          "less-loader"
         ]
       },
       {
         test: /\.(eot|woff|woff2|ttf|svg|png|jpe?g|gif)(\?\S*)?$/,
-        loader: 'url-loader',
+        loader: "url-loader",
         options: {
-          limit: '300000',
-          name: '[name].[ext]',
-          root: '.'
+          limit: "300000",
+          name: "[name].[ext]",
+          root: "."
         }
       }
     ]
@@ -56,7 +56,7 @@ export const webpackDistConfig = {
 export function webpackDistCompiler (callback) {
   const compiler = webpack (webpackDistConfig);
   compiler.run ((error, stats) => {
-    console.log ('Successfully bundled \'dist\'');
+    console.log ("Successfully bundled 'dist'");
     console.log (stats.toString ({ chunks: false, colors: true }));
     if (callback) callback ();
   });
@@ -65,7 +65,7 @@ export function webpackDistCompiler (callback) {
 export function webpackDistWatcher () {
   const compiler = webpack (webpackDistConfig);
   return compiler.watch ({}, (error, stats) => {
-    console.log ('Successfully bundled \'dist\'');
+    console.log ("Successfully bundled 'dist'");
     console.log (stats.toString ({ chunks: false, colors: true }));
   });
 }
